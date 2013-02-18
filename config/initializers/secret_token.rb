7f2e55ba3e48b2bd4f93b1b836a79478c6c30e34
Gledson Cruz <gledson.cruz@gmail.com>
2013-02-18 10:49:56 -0300

    Commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fiadonet::Application.config.secret_token = '7463ffff607f21d7eaaeebb22e9a40a4d2fdfd722dda3500ddf3532a3e9432d0a65463003d939c0cad4cd3d01cd8a73d299ec09243c8d82362db411e60e696db'
