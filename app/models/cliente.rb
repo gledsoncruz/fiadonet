8faf9171f7a04518bd825e2bd50976631d0f7fca
Gledson Cruz <gledson.cruz@gmail.com>
2013-02-19 17:51:14 -0300

    Acertando mensagens de erros

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -4,4 +4,8 @@ class Cliente < ActiveRecord::Base
   has_many :consumos
   has_many :produtos, :through => :consumos
   
+  validates_presence_of :cel, :email, :nome, :tel
+  validates_format_of :email, :with => /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
+  validates_uniqueness_of :email
+  
 end
