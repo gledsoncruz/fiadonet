e6b2b4a6df92898bc3b543f8a302057b6c5a21b0
Gledson Cruz <gledson.cruz@gmail.com>
2013-03-01 17:31:05 -0300

    Adicionando campo na tabela usuario

--- config/environment.rb
+++ config/environment.rb
@@ -7,3 +7,4 @@ Fiadonet::Application.initialize!
 Time::DATE_FORMATS[:data_br] = "%d/%m/%Y"
 Time::DATE_FORMATS[:dia_semana] = "%A"
 Time::DATE_FORMATS[:hora] = "%H:%M horas"
+ Time::DATE_FORMATS[:logado] = "%b, %m %Y - %I:%M%p"
