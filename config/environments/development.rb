4eb58cb675e0af33b593e24fe4b48c314b9f380a
Gledson Cruz <gledson.cruz@gmail.com>
2013-03-04 15:33:34 -0300

    Configurando email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -48,7 +48,7 @@ Fiadonet::Application.configure do
     :port => 587,
     :domain => "gmail.com",
     :user_name => "epdgeoprocessamento@gmail.com",
-    :password => "@siggeo2012",
+    :password => "********",
     :authentication => :login,
     :enable_starttls_auto => true
   }
